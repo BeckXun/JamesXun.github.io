744a3793bed1a954cdcf6e6bf8e26497a19945b9
fengfengxia <xjf_pro@163.com>
2017-01-17 17:35:39 +0800

    map tag

--- APP/es6/map.js
+++ APP/es6/map.js
@@ -16,12 +16,16 @@ var app = new Vue({
       window.location.reload();
     },
     render: function(option){
+
       // 百度地图API功能
+
     	let map = new BMap.Map("map");    // 创建Map实例(id="map")
-    	map.centerAndZoom(new BMap.Point(option.longitude, option.latitude), 16);  // 初始化地图,设置中心点坐标和地图级别
-    	// map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
-    	map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
-    	map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+      let point = new BMap.Point(option.longitude, option.latitude);
+      let marker = new BMap.Marker(point);
+    	map.centerAndZoom(point, 16);
+    	map.addOverlay(marker);
+      map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+
     }
   }
 });
