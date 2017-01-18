8bfbd1c1af3c118fa2285345250cf1d466c5ea8c
fengfengxia <xjf_pro@163.com>
2017-01-18 13:31:45 +0800

    ...

--- APP/js/map.js
+++ APP/js/map.js
@@ -18,15 +18,12 @@ var app = new Vue({
     render: function render(option) {
 
       // 百度地图API功能
-      var sContent = "<h4 style='margin:0 0 5px 0;padding:0.2em 0'>天安门</h4>" + "<img style='float:right;margin:4px' id='imgDemo' src='http://app.baidu.com/map/images/tiananmen.jpg' width='139' height='104' title='天安门'/>" + "<p style='margin:0;line-height:1.5;font-size:13px;text-indent:2em'>天安门坐落在中国北京市中心,故宫的南侧,与天安门广场隔长安街相望,是清朝皇城的大门...</p>";
       var map = new BMap.Map("map"); // 创建Map实例(id="map")
       var point = new BMap.Point(option.longitude, option.latitude);
       var marker = new BMap.Marker(point);
-      var infoWindow = new BMap.InfoWindow(sContent); // 创建信息窗口对象
       map.centerAndZoom(point, 16);
       map.addOverlay(marker);
       map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
-      marker.openInfoWindow(infoWindow); //图片加载完毕重绘infowind
     }
   }
 });
