b25f1ccd41548116c606ac091d30c8a8d87b6e59
JamesXun <xjf_pro@163.com>
2017-02-05 16:31:11 +0800

    ...

--- APP/js/carInfo.js
+++ APP/js/carInfo.js
@@ -49,19 +49,56 @@ var app = new Vue({
         },
         success: function success(data) {
           console.log(data);
+          // var baseData = {
+          //   default:  true,
+          //   detil: [
+          //     '品牌：重汽',
+          //     '购车日期：2016-01-01'
+          //   ]
+          // },
+          // carData = {
+          //   default:  true,
+          //   detil: [
+          //     '设备号：7001080',
+          //     '设备SIM卡号：7001080',
+          //     '设备SIM卡余额：85.15元'
+          //   ]
+          // };
+          // that.cacheInfo.base.push(baseData);
+          // that.cacheInfo.car.push(carData);
         }
       });
-      var baseData = {
+
+      this.cacheInfo.base = [{
         default: true,
         detil: ['品牌：重汽', '购车日期：2016-01-01']
-      },
-          carData = {
+      }, {
         default: true,
         detil: ['设备号：7001080', '设备SIM卡号：7001080', '设备SIM卡余额：85.15元']
-      };
-      that.cacheInfo.base.push(baseData);
-      that.cacheInfo.car.push(carData);
-      console.log(that.cacheInfo);
+      }, {
+        default: true,
+        detil: ['入网时间：2016-01-01  14:00:00', 'GPS最后回传时间：2016-01-01  14:00:00']
+      }, {
+        default: true,
+        detil: ['安装位置：主车', '硬件版本：V5.56_Z_HD10.A_BLDR3.05']
+      }, {
+        default: false,
+        detil: ['驾驶评分：'],
+        score: [1, 1, 1, 1, 0.5]
+      }];
+      this.cacheInfo.car = [{
+        default: true,
+        detil: ['设备号：7001080', '设备SIM卡号：7001080', '设备SIM卡余额：85.15元']
+      }, {
+        default: true,
+        detil: ['品牌：重汽', '购车日期：2016-01-01']
+      }, {
+        default: true,
+        detil: ['安装位置：主车', '硬件版本：V5.56_Z_HD10.A_BLDR3.05']
+      }, {
+        default: true,
+        detil: ['入网时间：2016-01-01  14:00:00', 'GPS最后回传时间：2016-01-01  14:00:00']
+      }];
     },
     tap: function tap(type) {
       if (type === 'baseInfo') {
